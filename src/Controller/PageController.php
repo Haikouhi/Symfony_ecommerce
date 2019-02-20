502c2738a52c050c8ebb526bc416010291fb1bab
= <=>
2019-02-20 16:38:59 +0100

    adding pages

---
+++ src/Controller/PageController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PageController extends AbstractController
+{
+    /**
+     * @Route("/", name="accueil")
+     */
+    public function index()
+    {
+        return $this->redirectToRoute('client_produit');
+    }
+}
--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -4,7 +4,8 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted; 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Component\Security\Core\Security; 
 
 class UserController extends AbstractController
 {
@@ -13,10 +14,13 @@ class UserController extends AbstractController
      * @Route("/profil", name="user_profil")
      * @IsGranted("ROLE_USER")
      */
-    public function index()
+    public function index(Security $security)
     {
+        $user = $security->getUser();
+
         return $this->render('user/index.html.twig', [
             'controller_name' => 'UserController',
+            'user' => $user
         ]);
     }
 }
